e49ca6d7c81f47879d595fcdfc3d501de187030c
sebastian <slmarcasm@gmail.com>
2026-04-08 18:47:38 -0500

    subiendo archivos

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prom4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, promedio;
+            Console.Write("Nota 1: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 2: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 3: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            promedio = (n1 + n2 + n3) / 3;
+
+            if (promedio >= 11)
+                Console.WriteLine("Aprobado con: " + promedio);
+            else
+                Console.WriteLine("Desaprobado con: " + promedio); 
+            Console.ReadKey();
+
+
+        }
+    }
+}
